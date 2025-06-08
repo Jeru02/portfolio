450dadf8a3ab868915619b15a51e8a3a82a85f89
Jeru02 <boatengj006472@gmail.com>
2025-06-08 15:38:10 +0100

    style project card

--- portfolio/src/components/ProjectsContainer.jsx
+++ portfolio/src/components/ProjectsContainer.jsx
@@ -6,56 +6,187 @@ const ProjectsContainer = () => {
       <h1 id="ProjectsContainer-title">Projects</h1>
       <div className="project-grid">
 
+        {/* project template below */}
+
         <div className="project-card">
           <div className="gradient">
             <div className="project-info">
               <h1 id="project-title">Papaya </h1>
               <div id="project-stack">
+                <div className="project-stack-card">
+                  <img
+                    src="/src/assets/project-tech/React-grey.png"
+                    alt=""
+                    className="tech-stack-img"
+                  />
+                  <p>React</p>
+                </div>
 
-              <div className="project-stack-card">
-              <img src="/src/assets/project-tech/React-grey.png" alt="" className="tech-stack-img"/>
-              <p>Python</p>
+                <div className="project-stack-card">
+                  <img
+                    src="/src/assets/project-tech/Node-grey.png"
+                    alt=""
+                    className="tech-stack-img"
+                  />
+                  <p>Node</p>
+                </div>
 
-
+                <div className="project-stack-card">
+                  <img
+                    src="/src/assets/project-tech/Express-grey.png"
+                    alt=""
+                    className="tech-stack-img"
+                  />
+                  <p>Express</p>
+                </div>
               </div>
-
-              <div className="project-stack-card">
-              <img src="/src/assets/project-tech/React-grey.png" alt="" className="tech-stack-img"/>
-              <p>Python</p>
-              </div>
-
-              <div className="project-stack-card">
-              <img src="/src/assets/project-tech/React-grey.png" alt="" className="tech-stack-img"/>
-              <p>Python</p>
-              </div>
-
-
-              </div>
-              <p id="project-about">Specialsed meal delivery serivice for women with PCOS</p>
+              <p id="project-about">
+                Specialsed meal delivery serivice for women with PCOS
+              </p>
 
               <div className="button-container">
-          <button id="read-more-button">Read more</button>
-
+                <button id="read-more-button">Read more</button>
               </div>
-            
             </div>
-            
           </div>
           <img src="src/assets/papaya.png" alt="" className="project-image" />
-          
         </div>
 
+        {/* project template below */}
 
         <div className="project-card">
-          <div className="gradient"></div>
+          <div className="gradient">
+            <div className="project-info">
+              <h1 id="project-title">Papaya </h1>
+              <div id="project-stack">
+                <div className="project-stack-card">
+                  <img
+                    src="/src/assets/project-tech/React-grey.png"
+                    alt=""
+                    className="tech-stack-img"
+                  />
+                  <p>React</p>
+                </div>
+
+                <div className="project-stack-card">
+                  <img
+                    src="/src/assets/project-tech/Node-grey.png"
+                    alt=""
+                    className="tech-stack-img"
+                  />
+                  <p>Node</p>
+                </div>
+
+                <div className="project-stack-card">
+                  <img
+                    src="/src/assets/project-tech/Express-grey.png"
+                    alt=""
+                    className="tech-stack-img"
+                  />
+                  <p>Express</p>
+                </div>
+              </div>
+              <p id="project-about">
+                Specialsed meal delivery serivice for women with PCOS
+              </p>
+
+              <div className="button-container">
+                <button id="read-more-button">Read more</button>
+              </div>
+            </div>
+          </div>
           <img src="src/assets/papaya.png" alt="" className="project-image" />
         </div>
+
+        {/* project template below */}
+
         <div className="project-card">
-          <div className="gradient"></div>
+          <div className="gradient">
+            <div className="project-info">
+              <h1 id="project-title">Papaya </h1>
+              <div id="project-stack">
+                <div className="project-stack-card">
+                  <img
+                    src="/src/assets/project-tech/React-grey.png"
+                    alt=""
+                    className="tech-stack-img"
+                  />
+                  <p>React</p>
+                </div>
+
+                <div className="project-stack-card">
+                  <img
+                    src="/src/assets/project-tech/Node-grey.png"
+                    alt=""
+                    className="tech-stack-img"
+                  />
+                  <p>Node</p>
+                </div>
+
+                <div className="project-stack-card">
+                  <img
+                    src="/src/assets/project-tech/Express-grey.png"
+                    alt=""
+                    className="tech-stack-img"
+                  />
+                  <p>Express</p>
+                </div>
+              </div>
+              <p id="project-about">
+                Specialsed meal delivery serivice for women with PCOS
+              </p>
+
+              <div className="button-container">
+                <button id="read-more-button">Read more</button>
+              </div>
+            </div>
+          </div>
           <img src="src/assets/papaya.png" alt="" className="project-image" />
         </div>
+
+        {/* project template below */}
+        
         <div className="project-card">
-          <div className="gradient"></div>
+          <div className="gradient">
+            <div className="project-info">
+              <h1 id="project-title">Papaya </h1>
+              <div id="project-stack">
+                <div className="project-stack-card">
+                  <img
+                    src="/src/assets/project-tech/React-grey.png"
+                    alt=""
+                    className="tech-stack-img"
+                  />
+                  <p>React</p>
+                </div>
+
+                <div className="project-stack-card">
+                  <img
+                    src="/src/assets/project-tech/Node-grey.png"
+                    alt=""
+                    className="tech-stack-img"
+                  />
+                  <p>Node</p>
+                </div>
+
+                <div className="project-stack-card">
+                  <img
+                    src="/src/assets/project-tech/Express-grey.png"
+                    alt=""
+                    className="tech-stack-img"
+                  />
+                  <p>Express</p>
+                </div>
+              </div>
+              <p id="project-about">
+                Specialsed meal delivery serivice for women with PCOS
+              </p>
+
+              <div className="button-container">
+                <button id="read-more-button">Read more</button>
+              </div>
+            </div>
+          </div>
           <img src="src/assets/papaya.png" alt="" className="project-image" />
         </div>
       </div>
