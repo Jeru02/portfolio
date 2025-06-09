81cfc7f2e0b51cc676e8c221b85b88d5a8837836
Jeru02 <boatengj006472@gmail.com>
2025-06-09 23:30:24 +0100

    make bio more concise

--- portfolio/src/components/AboutMe.jsx
+++ portfolio/src/components/AboutMe.jsx
@@ -1,42 +1,31 @@
 import ContactMeButton from "./ContactMeButton";
-import "../css/AboutMe.css"
+import "../css/AboutMe.css";
 import ContactLinks from "./ContactLinks";
 
-
 import Lottie from "lottie-react";
-import LottieAnimation from "../assets/Lotties/aura.json"
-
-
+import LottieAnimation from "../assets/Lotties/aura.json";
 
 const AboutMe = () => {
   return (
-    <div className="aboutMeContainer" >
+    <div className="aboutMeContainer">
       <div className="title">
-        <h1>Hi, I'm <span className="blue-text">Jeru</span></h1>
-        <h1>I build <span className="blue-text">Full stack</span> web applications</h1>
+        <h1>
+          Hi, I'm <span className="blue-text">Jeru</span>
+        </h1>
+        <h1>
+          I build <span className="blue-text">Full stack</span> web applications
+        </h1>
 
-       
         <p id="location-text">⚲ Bishops Stortford, United Kingdom</p>
-        
       </div>
 
       <p id="aboutMeDescription">
-
-
-
-
-
-        I'm a creative problem-solver with a background in computer science,
-        music production, and events management. I’m passionate about using
-        technology and culture to build innovative solutions and connect people.
-       
+        A computer science graduate with a background in music production and
+        events management.
       </p>
 
-
-    
       <ContactMeButton />
       <ContactLinks />
-
     </div>
   );
 };
