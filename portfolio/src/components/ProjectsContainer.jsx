6c239e719030aba0f2ef8e72334fdeca20e505d2
Jeru02 <boatengj006472@gmail.com>
2025-06-17 15:48:35 +0100

    add my projects and update tech stack

--- portfolio/src/components/ProjectsContainer.jsx
+++ portfolio/src/components/ProjectsContainer.jsx
@@ -5,7 +5,7 @@ const ProjectsContainer = () => {
     <>
       <h1 id="ProjectsContainer-title">Projects</h1>
       <div className="project-grid">
-        {/* project template below */}
+        {/* Papapya  */}
 
         <div className="project-card">
           <div className="gradient">
@@ -28,6 +28,15 @@ const ProjectsContainer = () => {
                     className="tech-stack-img"
                   />
                   <p>Node</p>
+                </div>
+
+                <div className="project-stack-card">
+                  <img
+                    src="/src/assets/project-tech/TypeScript-grey.png"
+                    alt=""
+                    className="tech-stack-img"
+                  />
+                  <p>TypeScript</p>
                 </div>
 
                 <div className="project-stack-card">
@@ -51,12 +60,12 @@ const ProjectsContainer = () => {
           <img src="src/assets/papaya.png" alt="" className="project-image" />
         </div>
 
-        {/* project template below */}
+        {/* Plantity */}
 
         <div className="project-card">
           <div className="gradient">
             <div className="project-info">
-              <h1 id="project-title">Papaya </h1>
+              <h1 id="project-title">Plantify </h1>
               <div id="project-stack">
                 <div className="project-stack-card">
                   <img
@@ -64,7 +73,7 @@ const ProjectsContainer = () => {
                     alt=""
                     className="tech-stack-img"
                   />
-                  <p>React</p>
+                  <p>React Native</p>
                 </div>
 
                 <div className="project-stack-card">
@@ -74,6 +83,15 @@ const ProjectsContainer = () => {
                     className="tech-stack-img"
                   />
                   <p>Node</p>
+                </div>
+
+                <div className="project-stack-card">
+                  <img
+                    src="/src/assets/project-tech/TypeScript-grey.png"
+                    alt=""
+                    className="tech-stack-img"
+                  />
+                  <p>TypeScript</p>
                 </div>
 
                 <div className="project-stack-card">
@@ -94,41 +112,33 @@ const ProjectsContainer = () => {
               </div>
             </div>
           </div>
-          <img src="src/assets/papaya.png" alt="" className="project-image" />
+          <img src="src/assets/plantify.png" alt="" className="project-image" />
         </div>
 
-        {/* project template below */}
+        {/* Audio Detective */}
 
         <div className="project-card">
           <div className="gradient">
             <div className="project-info">
-              <h1 id="project-title">Papaya </h1>
+              <h1 id="project-title">Audio Detective </h1>
               <div id="project-stack">
                 <div className="project-stack-card">
                   <img
-                    src="/src/assets/project-tech/React-grey.png"
+                    src="/src/assets/project-tech/java-grey.png"
                     alt=""
                     className="tech-stack-img"
+                    id = "java-tech-stack-icon"
                   />
-                  <p>React</p>
+                  <p>Java</p>
                 </div>
-
                 <div className="project-stack-card">
                   <img
-                    src="/src/assets/project-tech/Node-grey.png"
+                    src="/src/assets/project-tech/Android-studio-grey.png"
                     alt=""
                     className="tech-stack-img"
+                    id="android-tech-stack-icon"
                   />
-                  <p>Node</p>
-                </div>
-
-                <div className="project-stack-card">
-                  <img
-                    src="/src/assets/project-tech/Express-grey.png"
-                    alt=""
-                    className="tech-stack-img"
-                  />
-                  <p>Express</p>
+                  <p>Android studio</p>
                 </div>
               </div>
               <p id="project-about">
@@ -140,15 +150,15 @@ const ProjectsContainer = () => {
               </div>
             </div>
           </div>
-          <img src="src/assets/papaya.png" alt="" className="project-image" />
+          <img src="src/assets/audioDetective.png" alt="" className="project-image" />
         </div>
 
-        {/* project template below */}
+        {/* last project */}
 
         <div className="project-card">
           <div className="gradient">
             <div className="project-info">
-              <h1 id="project-title">Papaya </h1>
+              <h1 id="project-title">News Social Media </h1>
               <div id="project-stack">
                 <div className="project-stack-card">
                   <img
@@ -186,7 +196,7 @@ const ProjectsContainer = () => {
               </div>
             </div>
           </div>
-          <img src="src/assets/papaya.png" alt="" className="project-image" />
+          <img src="src/assets/NCnews.png" alt="" className="project-image" />
         </div>
       </div>
     </>
